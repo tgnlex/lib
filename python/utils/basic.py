a70037510e351f1490876b4113267826e6644219
tgnlex <alexanderwebber76@gmail.com>
2026-07-29 17:51:28 -0400

    auto-commit

--- python/utils/basic.py
+++ python/utils/basic.py
@@ -1,14 +1,38 @@
 
 
 def noop():
+    """ does absolutely nothing """
     pass
 
 def pass():
+    """ always returns true """
     return True
 
 def fail():
+    """ always returns false """
     return False
 
 def swap(x, y):
+    """ swaps the order of 2 values """
     return y, x
+
+def loop(fn):
+    """ runs a function without arguments indefinitely until cancled """
+    while True:
+        fn()
+
+def loop_with_args(fn, *args):
+    """ runs a function with arguments indefinitely until cancled """
+    while True:
+        fn(*args)
+
+def insert(dictionary, key, value):
+    """ inserts an mapping into a dictionary """
+    dictionary[key] = value
+
+
+def doc(fn):
+    """ gets the docstring of a function """
+    print(fn.__doc__)
+
 
--- python/utils/math.py
+++ python/utils/math.py
@@ -1,6 +1,10 @@
-
-
+import random
 def factorial(n):
+    """ 
+        takes a number as a parameter
+        and multiplies it by every whole number 
+        below it down to one.
+    """
     product = 1
     for i in range(2, n + 1):
         product *= i 
@@ -8,10 +12,23 @@ def factorial(n):
 
 
 def adder(x):
+    """ 
+        takes a number as a parameter and 
+        returns an adder function that
+        takes also takes a number as a parameter
+        and adds it to the original parameter
+    """
     def func(y):
         return x + y
     return func
 
 def doubler(iterable):
+    """ takes an iterable and doubles it """
     for i in iterable:
         yield i + i
+
+
+def roll_dice():
+    """ Returns a random integer between 1 and 6 """
+    result = random.randrange(1, 6)
+    return result
